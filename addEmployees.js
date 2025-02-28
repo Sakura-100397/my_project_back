2b1d638a9555b1b53b6460735a5912986a74c371
Sakura-100397 <sakura.nagai@growship.com>
2025-02-28 14:07:32 +0900

    Add files via upload

---
+++ addEmployees.js
@@ -0,0 +1,20 @@
+import connection from '../back/db.mjs';
+
+const id = '0001';
+const name = '永井さくら';
+const address =  '東京都杉並区';
+const mail = 'sakura@example.com';
+const phone_number = '08012345678';
+const position = 'Analyst';
+const password = 'password123';
+
+const query = `INSERT INTO employees (id,name,address,mail,phone_number,position,password)  
+                VALUES (?,?,?,?,?,?,?)`;
+
+connection.query(query, [id,name,address,mail,phone_number,position,password], (err, result) => {   
+    if(err){    
+        console.error('エラーが発生しました： ' + err.stack);
+        return;
+    }
+    console.log('成功しました: ' + result.insertId);
+});
