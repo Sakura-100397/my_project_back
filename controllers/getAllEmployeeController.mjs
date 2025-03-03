82659d6e353b8549ce5ee248e335b801e084d23d
Sakura Nagai <sakura.nagai@growship.com>
2025-03-03 10:20:28 +0900

    一覧の表示

--- controllers/getAllEmployeeController.mjs
+++ controllers/getAllEmployeeController.mjs
@@ -1,11 +1,9 @@
-import mysql from 'mysql2';
 import db from '../db.mjs';
-
 
 export const getAllEmployees = (req, res) => {
     db.query('SELECT * FROM employees', (err, results) => {
         if (err) {
-            res.status(500).json({ message: 'DB Error', error: err });
+            res.status(400).json({ message: 'データを取得できませんでした。', error: err });
         } else {
             res.status(200).json(results);  
         }
