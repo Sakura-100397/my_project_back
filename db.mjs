66aef50a5c52d9b6bde1c92a7d1c65fd9b249121
Sakura Nagai <sakura.nagai@growship.com>
2025-03-05 09:09:39 +0900

    dbの修正

--- db.mjs
+++ db.mjs
@@ -6,10 +6,10 @@ dotenv.config();
 const createDbConnection = async () => {   
     try{    
     const connection = await mysql.createConnection({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
 });
     return connection ;
 }catch (err) {  
